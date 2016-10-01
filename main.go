58685723352ad3159db04627149bcf5ccee82eb5
Samuel Bisciglia <samuelbisciglia@Samuels-MacBook-Pro.local>
2016-09-30 19:21:10 -0500

    fixed go routine logic

--- main.go
+++ main.go
@@ -1,17 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"sync"
+	"time"
 
 	"github.com/sbiscigl/phonenumberperm/intstack"
 	"github.com/sbiscigl/phonenumberperm/permutations"
 )
-
-// func consumerFunc(ch chan string) {
-// 	for {
-// 		fmt.Println(<-ch)
-// 	}
-// }
 
 var wg sync.WaitGroup
 
@@ -19,17 +15,15 @@ func main() {
 	num := []int{2, 7, 1, 4, 5, 5, 2}
 
 	stack := intstack.New(num)
-	// start := time.Now()
-	//permutations.CalcWords(stack, "")
-	// diff := time.Since(start).Nanoseconds()
-	// fmt.Printf("time elapsed : %d\n", diff)
+	start := time.Now()
+	permutations.CalcWords(stack, "")
+	diff := time.Since(start).Nanoseconds()
+	fmt.Printf("time elapsed : %d\n", diff)
 
-	//start := time.Now()
-	permutationChannel := make(chan string)
+	newStart := time.Now()
 	wg.Add(1)
-	go permutations.ThreadSafeCalcWords(stack, "", permutationChannel, &wg)
+	go permutations.ThreadSafeCalcWords(stack, "", &wg)
 	wg.Wait()
-	//consumerFunc(permutationChannel)
-	//diff := time.Since(start).Nanoseconds()
-	//fmt.Printf("time elapsed : %d\n", diff)
+	newDiff := time.Since(newStart).Nanoseconds()
+	fmt.Printf("time elapsed : %d\n", newDiff)
 }
